31d4cabe5b2f0200bf38db77b710261de4f58180
stacksonstack <staceycarrillo44@gmail.com>
2021-05-05 23:23:36 -0400

    incorporated newsletter functionality

--- src/Components/BeveragePreview.js
+++ src/Components/BeveragePreview.js
@@ -4,14 +4,17 @@ import { NavLink } from "react-router-dom";
 function BeveragePreview({name, price, image, beverageId }) {
 
   return (
-    <div className="beverage-preview">
+    <>
       <NavLink to={`/beverages/${beverageId}`}>
+        <div className="beverage-preview">
         <h3>{name}</h3>
-      </NavLink>
+      
       
       <img id="beverage-img" alt={name} src={`http://localhost:1337${image}`}/>
       <p>${price}</p>
     </div>
+    </NavLink>
+    </>
   );
 }
 
--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -1,14 +1,14 @@
 import React from "react";
+import Mailchimp from 'react-mailchimp-form'
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+
 
 function Newsletter() {
   return (
     <>
       <h2>Stay Tuned!</h2>
       <p>Sign up to get updates about new edibles and drinks!</p>
-      <form>
-        <input placeholder="Enter Email Address" />
-      </form>
-      <button> Join The List</button>
+      <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
     </>
   );
 }
